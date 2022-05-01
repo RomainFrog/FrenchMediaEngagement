3aabb2ac8f8c785cdb152e18e4712ffe58e886a3
Nicolas Scheidler <nicolas.scheidler@etu.utc.fr>
2022-05-01 17:20:11 +0200

    Get account info and tweets info

--- twitter/connect.py
+++ twitter/connect.py
@@ -15,26 +15,23 @@ client = tweepy.Client(config['bearer_token'])
 #Generating a list of account:
 
 usernames = ['lemondefr', 'mediavenir']
-results = {x.username : [x.id, x.description, x.public_metrics, x.verified, x.location] for x in client.get_users(usernames = usernames, user_fields=['description', 'public_metrics', 'verified', 'location']).data}
+results = {x.username : {'id' : int(x.id), 'description' : x.description, 'public_metrics' : x.public_metrics, 'verified' : x.verified, 'location' : x.location} for x in client.get_users(usernames = usernames, user_fields=['description', 'public_metrics', 'verified', 'location']).data}
 
 with open('accounts.json', 'w') as f:
 	json.dump(results, f)
 
 #tweepy get 5 tweets from account
-#Le Monde FR
-#for user in results:
-#	result[user] = []
-#	result[user].append(client.get_users(24744541))
-#info = client.get_users(24744541)
+tweets = {}
+for user in results.values():
+	tweets[user['id']] = client.get_users_tweets(user['id'], max_results=5, media_fields=['type', 'public_metrics'], tweet_fields=['created_at', 'geo', 'public_metrics', 'referenced_tweets'], user_auth=False)[0]
 
-"""
-result = client.get_users_tweets(24744541, max_results=5, user_auth=False)
-data = []
-for tweet in result[0]:
-	data.append(tweet.data)
+
+data = {}
+for user_id, user in tweets.items():
+	data[user_id] = []
+	for tweet in user:
+		data[user_id].append(tweet.data)
 
 #save data json to file
 with open('tweets.json', 'w') as f:
 	json.dump(data, f)
-
-"""
