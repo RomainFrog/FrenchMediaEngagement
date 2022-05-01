9fe2259c802941810fe507cb5076ff9ae6e3091b
Nicolas Scheidler <nicolas.scheidler@etu.utc.fr>
2022-05-01 16:30:30 +0200

    Get account data

--- twitter/connect.py
+++ twitter/connect.py
@@ -10,12 +10,25 @@ with open('.env') as json_file:
 	data = json.load(json_file)
 	config['bearer_token'] = data['BEARER_TOKEN']
 
-#print(config)
 client = tweepy.Client(config['bearer_token'])
 
-#tweepy get 5 tweets from account
-result = client.get_users_tweets(24744541, max_results=5, user_auth=False)
+#Generating a list of account:
 
+usernames = ['lemondefr', 'mediavenir']
+results = {x.username : [x.id, x.description, x.public_metrics, x.verified, x.location] for x in client.get_users(usernames = usernames, user_fields=['description', 'public_metrics', 'verified', 'location']).data}
+
+with open('accounts.json', 'w') as f:
+	json.dump(results, f)
+
+#tweepy get 5 tweets from account
+#Le Monde FR
+#for user in results:
+#	result[user] = []
+#	result[user].append(client.get_users(24744541))
+#info = client.get_users(24744541)
+
+"""
+result = client.get_users_tweets(24744541, max_results=5, user_auth=False)
 data = []
 for tweet in result[0]:
 	data.append(tweet.data)
@@ -23,3 +36,5 @@ for tweet in result[0]:
 #save data json to file
 with open('tweets.json', 'w') as f:
 	json.dump(data, f)
+
+"""
