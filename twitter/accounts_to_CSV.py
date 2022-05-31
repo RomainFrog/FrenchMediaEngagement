61689b8089226feb256f8cf465a19ae7d1e169bd
Nicolas Scheidler <nicolas.scheidler@etu.utc.fr>
2022-05-31 15:29:19 +0200

    Little modification

--- twitter/accounts_to_CSV.py
+++ twitter/accounts_to_CSV.py
@@ -7,4 +7,4 @@ with open('accounts.json') as json_file:
 
 with open('author.csv', 'w') as f:
 	for author, a_data in author_data.items():
-		f.write(f"{a_data['username']};{author};" + str({a_data['description'].replace('\n','//')}) + f";{a_data['public_metrics']['followers_count']}\n")
+		f.write(f"{a_data['username']};{author};" + str(a_data['description'].replace('\n','//')) + f";{a_data['public_metrics']['followers_count']}\n")
